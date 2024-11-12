7d929972586e4cfb2265e11aa693c4bf5382b158
Bjornl0f <kostykstefan@gmail.com>
2024-11-12 08:41:51 +0200

    Removed build folder from .gitignore

---
+++ build/generated/sources/annotationProcessor/java/main/com/softserve/mapper/ScheduleFullMapperImpl.java
@@ -0,0 +1,13 @@
+package com.softserve.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-10T00:10:34+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
+)
+@Component
+public class ScheduleFullMapperImpl implements ScheduleFullMapper {
+}
